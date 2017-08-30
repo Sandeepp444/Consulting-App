c1b2debab299bc624488ded8b6cbc522d06b6ea4
Sandeepp444 <30373459+Sandeepp444@users.noreply.github.com>
2017-08-29 21:19:39 -0500

    Delete webserver.js

--- webserver.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var rootpath =path.normalize(_dirname +'/../');
-app.use(express.static(rootPath + '/app'));
-app.listen(8000);
-console.log('Listening on port 8000...');
